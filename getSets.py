9e1c69b9b3b7197eec17824eca62ae00df684695
IdanHershcovich <idanhershcovich@gmail.com>
2021-02-01 16:45:12 -0500

    Added functions to read and write to JSON, sort by date and filter out unplayable card sets. Included JSON file with usable sets

--- getSets.py
+++ getSets.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 
 def getSetsJSON():
@@ -18,7 +19,6 @@ def getSetsJSON():
         },]
     """
 
-
     url = 'https://db.ygoprodeck.com/api/v7/cardsets.php'
     try:
         r = requests.get(url)
@@ -36,9 +36,44 @@ def getSetsJSON():
 def saveSets():
     """Saves the yugioh sets returned from getSetsJSON() to a local .json file to avoid API calls """
     sets = getSetsJSON()
-    with open('ygo-sets.json', 'w', encoding='utf-8') as f:
-        json.dump(sets, f, ensure_ascii=False, indent=4)
+    writeJSONFile(sets, 'ygo-sets.json')
+            
+def filterSets():
+    """Filters all the available sets to only exclude Speed Duel sets, promotional sets and Structure Decks"""
+    sets = readJSONFile('ygo-sets.json')
+    filtered = []
+    for cardset in sets:
+        if cardset['num_of_cards'] < 40 or "Speed Duel" in cardset['set_name'] or "Structure Deck" in cardset['set_name']:
+            continue
+        filtered.append(cardset)
+    writeJSONFile(filtered, 'ygo-sets.json')
 
+def readJSONFile(filename):
+    """Reads the local JSON file containing the Card Sets
 
+    Args:
+        filename (string): the name and path of the JSON file to read
 
-saveSets()
+    Returns:
+        [JSON]: JSON file data
+    """
+    with open(filename) as f:
+        data = json.load(f)
+    return data
+
+def writeJSONFile(data,filename):
+    """[Function to write to a specified JSON file]
+
+    Args:
+        data (JSON): The JSON data to write
+        filename (string): the name and path of the JSON file to write to
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    
+def sortSetsByDate():
+    """Grabs the date attribute of the JSON Card Sets and sorts them by date. Oldest to Newest"""
+    filename = 'ygo-sets.json'
+    sets = readJSONFile(filename)
+    sorted_sets = sorted(sets, key=lambda x: datetime.strptime(x['tcg_date'], "%Y-%m-%d"))
+    writeJSONFile(sorted_sets, filename)
